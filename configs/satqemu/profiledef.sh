5f0a8481f1fe7d8a93e2162fb143a7f1c8287e29
StarterX4 <starterx4@gmail.com>
2022-12-15 12:19:49 +0100

    Early porting phase

---
+++ configs/satqemu/profiledef.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="archlinuxrv64"
+iso_label="ARCH_RISCV64_$(date +%Y%m)"
+iso_publisher="StarterX4"
+iso_application="Generic Reference RISC-V Arch Linux Live/Rescue Disk"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('uefi-riscv64.systemd-boot.esp' 'uefi-riscv64.systemd-boot.eltorito')
+arch="aarch64"
+pacman_conf="pacman.conf"
+airootfs_image_type="erofs"
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'arm' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-zlz4hc,12')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+)
